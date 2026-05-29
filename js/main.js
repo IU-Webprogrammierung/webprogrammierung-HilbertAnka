324e47c5bbd902ea9f8c6e935207980990decda1
Anka Hilbert <anka.hilbert@iu-study.org>
2026-05-29 14:16:11 +0200

    refactor navbar into reusable component, add Fonts (Dancing Script, Montserrat), move imprint content to JSON translation files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+async function loadComponent(id, file) {
+
+    const response = await fetch(file);
+    const html = await response.text();
+
+    document.getElementById(id).innerHTML = html;
+}
+
+loadComponent("navbar", "components/navbar.html")
+    .then(() => loadLanguage(currentLanguage));
