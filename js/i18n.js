8c5ec38489ed180f15f343f277b4b0a9ebfbe018
Anka Hilbert <anka.hilbert@iu-study.org>
2026-05-29 12:00:05 +0200

    Implement basic i18n system with English and German translations
    - add en.json and de.json translation files
    - create i18n.js for dynamic language loading
    - implement language switch button
    - move static HTML text into translation JSON files

---
+++ js/i18n.js
@@ -0,0 +1,60 @@
+
+let currentLanguage = "en";
+
+// Code to load language and update content
+async function loadLanguage(lang) {
+
+    currentLanguage = lang;
+
+    const response = await fetch(`lang/${lang}.json`); // load JSON
+    const translation = await response.json(); // convert JSON to JavaScript -> text becomes an object
+
+    // get all keys from the translations object
+    // for each key: find matching HTML element and update its textContent
+    Object.keys(translation).forEach(key => { 
+
+        const element = document.getElementById(key);
+
+        if (element) {
+            element.textContent = translation[key];
+        }
+    
+
+    });
+
+    updateLanguageButton();
+}
+
+// language switch function
+function setLanguage(lang) {
+    localStorage.setItem("language", lang)
+    loadLanguage(lang);
+}
+
+
+//update text in language-button
+function updateLanguageButton() {
+
+    const button = document.getElementById("language-switch");
+
+    if(currentLanguage === "en") {
+
+        button.textContent = "DE",
+        button.onclick = () => setLanguage("de");
+
+    } else {
+
+        button.textContent = "EN",
+        button.onclick = () => setLanguage("en");
+    
+    }
+}
+
+// save selected language
+const savedLanguage = localStorage.getItem("language");
+
+        if (savedLanguage) {
+            loadLanguage(savedLanguage);
+        } else {
+            loadLanguage(currentLanguage);
+        }
